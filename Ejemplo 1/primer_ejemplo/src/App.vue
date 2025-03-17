ae29d82f7f12c1964f73dd52b6bcc3ac6ab49cf7
DavidSevillano <dvd.sevillano@gmail.com>
2025-03-17 18:01:35 +0100

    Pequeños cambios

--- Ejemplo 1/primer_ejemplo/src/App.vue
+++ Ejemplo 1/primer_ejemplo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Ejemplo ejemplísimo" msg2="Ejemplísimo 2"/>
   </div>
 </template>
 
